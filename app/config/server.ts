59e52669aa5b5b34a6e9409b2193740aeb58fea0
Vigilio <yonatanvigiliolavado09@gmail.com>
2023-07-26 22:23:33 -0500

    fixed

--- app/config/server.ts
+++ app/config/server.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import cors from "cors";
-import passport from "passport";
 import path from "path";
 import { ERROR_MIDDLEWARE, attachControllers } from "@decorators/express";
 import { enviroments } from "~/config/enviroments.config";
@@ -10,7 +8,6 @@ import { Container } from "@decorators/di";
 import { ServerErrorMiddleware } from "@vigilio/express-core/handler";
 import { logger } from "@vigilio/express-core/helpers";
 import { client } from "@vigilio/express-core/client";
-import { ExtractJwt, Strategy, StrategyOptions } from "passport-jwt";
 import { webRouters } from "~/routers/web";
 
 export class Server {
@@ -23,19 +20,10 @@ export class Server {
 	}
 
 	auth() {
-		const jwtOptions: StrategyOptions = {
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: enviroments.SECRET_JWT_KEY,
-		};
-		const strategy = new Strategy(jwtOptions, async (_jwtpayload, _done) => {
-			// done(null, user);
-		});
-
-		passport.use(strategy);
+	
 	}
 
 	middlewares() {
-		this.app.use(cors());
 		this.app.use(express.json());
 		this.app.use(express.static(path.resolve(__dirname, "..", "..", "public")));
 		this.app.set("view engine", "pug");
